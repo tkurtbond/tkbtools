8393fea39c4d53f12ecbb6efffb8ad6018288c3a
T. Kurt Bond <tkurtbond@gmail.com>
2023-04-10 15:38:43 -0400

    Another release from the author.

--- others/fmtroff/fmtroff.c
+++ others/fmtroff/fmtroff.c
@@ -1,52 +1,48 @@
-/* https://en.roquesor.com/Downloads/fmtroff.c */
 /* Copyright (c) 2022 Walter Alejandro Iglesias <wai@roquesor.com>.
  *
- * I called this 'fmtroff' just to distinguish it from the fmt(1)
- * utility already present in any unix-like OS, I chose the suffix
- * 'roff' because my main concern and motivation was to improve it for
- * using it to format my paragraphs within roff files.  Don't get
- * confused as someone who thought it was a "roff formatter" and
- * expected it would do anything in macro files, this is just a new
- * version of fmt, the only thing it does is line wrapping your
- * paragraphs, fmtroff won't modify anything in lines beginning with a
- * dot character unless you pass the option '-n' which is thought to
- * allow you *also* wrap those lines in other file types, like e-mail.
+ * Last update: Thu Apr  6 14:05:59 CEST 2023
  *
- * I'm a writer, I use GNU roff (groff) to edit my novels; having to
- * correct hundreds of pages and line wrapping my paragraphs in the
- * process I was always worried about those exceptions in which the
- * formatter didn't do what I expected.  I guess most people trust the
- * formatting paragraph features embedded in the popular advanced text
- * editors (what happens when you press 'gwip' on Vim or 'M-q' in GNU
- * Emacs), since they can format anything in any type of file, even
- * comments in code or quoted text in email, however "Jack of all
- * trades, master of none", goes the saying, neither they nor any of the
- * fmt versions I tried did the job in my roff files without subtle
- * (those that go unnoticed) unwanted side effects.  This motivated me
- * to write this program.  So, I took from other versions of fmt (*BSD
- * and GNU) the features I found useful, enhanced those features, and
- * added a new one, that's breaking sentences with a new line character
- * (option '-t'), as I understand is the convenient practice with troff.
+ * I called this 'fmtroff' to distinguish it from the fmt(1) utility
+ * already present in any unix-like OS, and because my main concern and
+ * motivation was to improve it for using it to line wrapping paragraphs
+ * within troff files, but it's just a new version of fmt (not a *roff
+ * formatter!)
+ *
+ * I use GNU roff (groff) to edit my novels (hundred of pages), I was
+ * always worried about those exceptions in which the line wrapper
+ * didn't do what I expected.  I guess most people trust the formatting
+ * paragraph features embedded in popular advanced text editors (what
+ * happens when you press 'gwip' on Vim or 'M-q' in GNU Emacs), since
+ * they can format anything in any type of file, even comments in code
+ * or quoted text in email, however "Jack of all trades, master of
+ * none", goes the saying, neither they nor any of the fmt versions I
+ * tried did the job without some slight unwanted side effect.  This
+ * motivated me to write this program.  So, I took from other versions
+ * of fmt (*BSD and GNU) the features I found useful, enhanced those
+ * features, and added a new one, that's breaking sentences with a new
+ * line character (option '-t'), as I understand is the convenient
+ * practice with troff files.
  *
  * It's important to notice that it ONLY SUPPORTS UTF-8.  When counting
- * columns it takes in care UTF-8 multi-byte characters.  Except at the
- * first line of each paragraph it always collapses consecutive spaces
- * or tabs and it's able to recognize initials and some abbreviations
- * followed by a period, it won't add an extra space in these cases (or
- * break the line with troff mode).  When you tell it to indent the
- * whole paragraph (option '-p') it copies the first line indentation
- * "as is", including tabs.
+ * columns it takes in care UTF-8 multi-byte characters.  It always
+ * collapses consecutive spaces or tabs (leaving the first line
+ * indentation untouched) and it's able to recognize initials and some
+ * abbreviations followed by a period, it won't add an extra space in
+ * these cases (or break the line with troff mode).  When you tell it to
+ * indent the whole paragraph (option '-p') it copies the first line
+ * indentation "as is", including tabs.
  *
  * Besides '.?!' it also considers UTF-8 ellipsis as an end of sentence
  * character.  It ignores any amount of quotes or parenthesis following
- * these characters.  To recognize sentence boundaries it also checks if
- * the following sentence begins with an uppercase letter and also
- * ignores opening quotes or parenthesis in this case.  You'll notice
- * that I added Spanish specific symbols to the opening sentence
- * recognition (Spanish quotes and inverted interrogation and
- * exclamation signs), I'd like to add also those needed by other
- * languages (to the 'op_quote' function), but Spanish and English are
- * the only languages I barely know. :-)
+ * these characters (for those who use the American English style).  To
+ * recognize sentence boundaries it also checks if the following
+ * sentence begins with an uppercase letter (which can disabled with the
+ * option '-l') and also ignores opening quotes or parenthesis in this
+ * case.  You'll notice that I added Spanish specific symbols to the
+ * opening sentence recognition (Spanish quotes and inverted
+ * interrogation and exclamation signs), I'd like to add also those
+ * needed by other languages (to the 'op_quote' function), but Spanish
+ * and English are the only languages I barely know. :-)
  */
 
 #include <err.h>
@@ -61,6 +57,7 @@ static int width = 72;		/* Default maximum columns */
 static int troff = 0;		/* Troff mode */
 static int dot = 1;		/* Ignore lines beginning with a dot */
 static int ind_par = 0;		/* Indent the whole paragraph */
+static int lcase = 0;		/* Allow sentences begin with low case */
 
 static void	filecopy(FILE *, FILE *);
 static int	clean_trailing(unsigned char s[], int len);
@@ -68,8 +65,8 @@ static void	join_lines(unsigned char s[], int len);
 static int	collapse_whitespace(unsigned char s[], int len);
 static int	separate_sentences(unsigned char s[], int len, int check);
 static int	wrap_lines(unsigned char s[], int len, int check);
-static int	cl_quotes(unsigned char s[]);
-static int	op_quotes(unsigned char s[]);
+static int	cl_quot(unsigned char s[]);
+static int	op_quot(unsigned char s[]);
 static int	spaces(unsigned char s[]);
 static int	word(unsigned char s[]);
 static int	utf8_count(unsigned char s[]);
@@ -81,13 +78,16 @@ main (int argc, char *argv[])
 	FILE *fp;
 	int option, user_width;
 
-	while ((option = getopt(argc, argv, "nhptw:")) != -1)
+	while ((option = getopt(argc, argv, "hlnptw:")) != -1)
 		switch (option) {
-		case 'n':
-			dot = 0;
-			break;
 		case 'h':
 			usage();
+			break;
+		case 'l':
+			lcase = 1;
+			break;
+		case 'n':
+			dot = 0;
 			break;
 		case 'p':
 			ind_par = 1;
@@ -234,11 +234,12 @@ join_lines(unsigned char s[], int len)
 			blankline = 0;
 
 		if ((troff || dot) &&
-		   ((r[n] == '\n' && r[n + 1] == '.') ||
-		    (r[n] == '.' && (n == 0 ||  r[n - 1] == '\n'))))
+		   ((r[n] == '\n' && (r[n + 1] == '.' || r[n + 1] == 0x27)) ||
+		   ((r[n] == '.' || r[n] == 0x27 ) &&
+		    (n == 0 ||  r[n - 1] == '\n'))))
 			dotline = 1;
 
-		if (dotline && r[n] != '.' &&
+		if (dotline && r[n] != '.' && r[n] != 0x27 &&
 		   (n == 0 || r[n - 1] == '\n'))
 			dotline = 0;
 
@@ -278,11 +279,12 @@ collapse_whitespace(unsigned char s[], int len)
 			blankline = 0;
 
 		if ((troff || dot) &&
-		   ((r[n] == '\n' && r[n + 1] == '.') ||
-		    (r[n] == '.' && (n == 0 ||  r[n - 1] == '\n'))))
+		   ((r[n] == '\n' && (r[n + 1] == '.' || r[n + 1] == 0x27)) ||
+		   ((r[n] == '.' || r[n] == 0x27 ) &&
+		    (n == 0 ||  r[n - 1] == '\n'))))
 			dotline = 1;
 
-		if (dotline && r[n] != '.' &&
+		if (dotline && r[n] != '.' && r[n] != 0x27 &&
 		   (n == 0 || r[n - 1] == '\n'))
 			dotline = 0;
 
@@ -333,67 +335,97 @@ separate_sentences(unsigned char s[], int len, int check)
 			blankline = 0;
 
 		if ((troff || dot) &&
-		   ((r[n] == '\n' && r[n + 1] == '.') ||
-		    (r[n] == '.' && (n == 0 ||  r[n - 1] == '\n'))))
+		   ((r[n] == '\n' && (r[n + 1] == '.' || r[n + 1] == 0x27)) ||
+		   ((r[n] == '.' || r[n] == 0x27 ) &&
+		    (n == 0 ||  r[n - 1] == '\n'))))
 			dotline = 1;
 
-		if (dotline && r[n] != '.' &&
+		if (dotline && r[n] != '.' && r[n] != 0x27 &&
 		   (n == 0 || r[n - 1] == '\n'))
 			dotline = 0;
 
 		if (dotline || blankline)
 			;
 		else if (r[n] == ' ' && n != 0 && n != len - 1) {
-			/*
-			 * Try to recognize and skip name initialisms
-			 * as Michel J. Fox
-			 */
+			/* Try to recognize and skip some initialisms */
 			if (n >= 3 && r[n - 1] == '.' &&
 			    r[n - 2] >= 'A' && r[n - 2] <= 'Z' &&
 			   (r[n - 3] == ' ' || r[n - 3] == '.'))
 				;
+
 			/*
-			 * Try to recognize and skip other initialisms
-			 * as F.B.I., i.e., Ph.D.
+			 * Skip ASCII ellipsis at the beggining of a
+			 * sentence
 			 */
-			else if (n >= 4 && r[n - 1] == '.' &&
-			       ((r[n - 2] >= 'a' && r[n - 2] <= 'z') ||
-			        (r[n - 2] >= 'A' && r[n - 2] <= 'Z')) &&
+			else if (n == 3 &&
+				 r[n - 1] == '.' &&
+				 r[n - 2] == '.' &&
+				 r[n - 3] == '.')
+				;
+			else if (n >= 4 &&
+				 r[n - 1] == '.' &&
+				 r[n - 2] == '.' &&
 				 r[n - 3] == '.' &&
-			       ((r[n - 4] >= 'a' && r[n - 4] <= 'z') ||
-			        (r[n - 4] >= 'A' && r[n - 4] <= 'Z')))
+				 r[n - 4] == ' ')
 				;
+
 			/*
-			 * Try to recognize and skip some abbreviations
-			 * as Mr., Mrs.
+			 * Skip commonly used title abbreviations (if
+			 * you wish to include here more from your
+			 * language take in mind that only those
+			 * followed by space + uppercase need this
+			 * filter.)
 			 */
+
+			/* Mr. Dr. Sr. */
 			else if (n >= 3 && r[n - 1] == '.' &&
-				 r[n - 2] >= 'a' && r[n - 2] <= 'z' &&
-				 r[n - 3] >= 'A' && r[n - 3] <= 'Z')
+				 r[n - 2] == 'r' &&
+				(r[n - 3] == 'D' || r[n - 3] == 'M' ||
+				 r[n - 3] == 'S'))
 				;
+			/* Ms. */
+			else if (n >= 3 && r[n - 1] == '.' &&
+				 r[n - 2] == 's' && r[n - 3] == 'M')
+				;
+			/* Mrs. */
 			else if (n >= 4 && r[n - 1] == '.' &&
-				 r[n - 2] >= 'a' && r[n - 2] <= 'z' &&
-				 r[n - 3] >= 'a' && r[n - 3] <= 'z' &&
-				 r[n - 4] >= 'A' && r[n - 4] <= 'Z')
+				 r[n - 2] == 's' &&
+				 r[n - 3] == 'r' &&
+				 r[n - 4] == 'M')
 				;
+			/* Dra. Sra. (Spanish) */
+			else if (n >= 4 && r[n - 1] == '.' &&
+				 r[n - 2] == 'a' &&
+				 r[n - 3] == 'r' &&
+				(r[n - 4] == 'D' || r[n - 4] == 'S'))
+				;
+			/* Prof. */
+			else if (n >= 5 && r[n - 1] == '.' &&
+				 r[n - 2] == 'f' &&
+				 r[n - 3] == 'o' &&
+				 r[n - 4] == 'r' &&
+				 r[n - 5] == 'P')
+				;
+
 			/* End of sentence */
 			else if (((n >= 3 &&
-				  (r[n + cl_quotes(&r[n - 1]) - 1] == '.' ||
-				   r[n + cl_quotes(&r[n - 1]) - 1] == '!' ||
-				   r[n + cl_quotes(&r[n - 1]) - 1] == '?' )) ||
-				/* ellipsis */
+				  (r[n + cl_quot(&r[n - 1]) - 1] == '.' ||
+				   r[n + cl_quot(&r[n - 1]) - 1] == '!' ||
+				   r[n + cl_quot(&r[n - 1]) - 1] == '?' )) ||
+				/* UFT-8 ellipsis */
 				  (n >= 4 &&
-				  (r[n + cl_quotes(&r[n - 1]) - 3] == 0xe2 &&
-				   r[n + cl_quotes(&r[n - 1]) - 2] == 0x80 &&
-				   r[n + cl_quotes(&r[n - 1]) - 1] == 0xa6)))
+				  (r[n + cl_quot(&r[n - 1]) - 3] == 0xe2 &&
+				   r[n + cl_quot(&r[n - 1]) - 2] == 0x80 &&
+				   r[n + cl_quot(&r[n - 1]) - 1] == 0xa6)))
 					&&
-				/* Begin of next sentence */
-				 ((r[n + op_quotes(&r[n + 1]) + 1] >= 'A' &&
-				   r[n + op_quotes(&r[n + 1]) + 1] <= 'Z') ||
+			/* Begin of next sentence */
+				 ( lcase ||
+				 ((r[n + op_quot(&r[n + 1]) + 1] >= 'A' &&
+				   r[n + op_quot(&r[n + 1]) + 1] <= 'Z') ||
 				/* Capital with tilde */
-				  (r[n + op_quotes(&r[n + 1]) + 1] == 0xc3 &&
-				   r[n + op_quotes(&r[n + 1]) + 2] >= 0x80 &&
-				   r[n + op_quotes(&r[n + 1]) + 2] <= 0x9d))) {
+				  (r[n + op_quot(&r[n + 1]) + 1] == 0xc3 &&
+				   r[n + op_quot(&r[n + 1]) + 2] >= 0x80 &&
+				   r[n + op_quot(&r[n + 1]) + 2] <= 0x9d)))) {
 				if (troff)
 					r[n] = '\n';
 				else {
@@ -447,11 +479,12 @@ wrap_lines(unsigned char s[], int len, int check)
 			blankline = 0;
 
 		if ((troff || dot) &&
-		   ((r[n] == '\n' && r[n + 1] == '.') ||
-		    (r[n] == '.' && (n == 0 ||  r[n - 1] == '\n'))))
+		   ((r[n] == '\n' && (r[n + 1] == '.' || r[n + 1] == 0x27)) ||
+		   ((r[n] == '.' || r[n] == 0x27 ) &&
+		    (n == 0 ||  r[n - 1] == '\n'))))
 			dotline = 1;
 
-		if (dotline && r[n] != '.' &&
+		if (dotline && r[n] != '.' && r[n] != 0x27 &&
 		   (n == 0 || r[n - 1] == '\n'))
 			dotline = 0;
 
@@ -549,7 +582,7 @@ wrap_lines(unsigned char s[], int len, int check)
 }
 
 static int
-cl_quotes(unsigned char s[])
+cl_quot(unsigned char s[])
 {
 	int i = 0;
 	while (s[i] != '.' && s[i] != '?' && s[i] != '!' && s[i] != 0xa6) {
@@ -566,7 +599,7 @@ cl_quotes(unsigned char s[])
 	return i;
 }
 static int
-op_quotes(unsigned char s[])
+op_quot(unsigned char s[])
 {
 	int i = 0;
 	while (s[i] != ' ' && s[i] != '\t' && s[i] != '\n') {
@@ -608,7 +641,10 @@ word(unsigned char s[])
 
 	while (s[i] != ' ' && s[i] != '\t' && s[i] != '\n' && s[i] != '\0') {
 		count -= utf8_count(&s[i]);
-		count++;
+		if (count > width)
+			count = 0;
+		else
+			count++;
 		i++;
 	}
 
@@ -655,7 +691,10 @@ usage(void)
 	fprintf(stderr,
 		"Usage: %s [-hnt] [-w width] [file ...]\n"
 		"  -h   print this help\n"
-		"  -n   format lines beginning with a dot character\n"
+		"  -l   allow sentences begin with a low case letter (you "
+		"may want to activate\n"
+		"         this with man pages)\n"
+		"  -n   format also lines beginning with a dot character\n"
 		"  -t   separate sentences with a new line (troff mode)\n"
 		"  -w   set maximum line width (default 72 columns)\n",
 		__progname);
