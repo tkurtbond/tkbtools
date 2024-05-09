b36c69716422caa0964ddafd68e3f33d6fb5f892
T. Kurt Bond <tkurtbond@gmail.com>
2024-05-09 14:07:45 -0400

    Added (* and *)

--- Src/Forth/Gforth/tkb.fs
+++ Src/Forth/Gforth/tkb.fs
@@ -1,2 +1,7 @@
 : POSSIBLY  ( "name" -- )  BL WORD FIND  ?dup AND IF  EXECUTE  THEN ;
 : anew  ( "name" -- )( Run: -- )  >in @ possibly  >in ! marker ;
+
+' { alias {: immediate
+locals-types definitions
+' } alias :}
+forth definitions
