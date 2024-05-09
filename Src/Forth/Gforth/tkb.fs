238f7cbd295984b23a740efd2e47d748b4ee890b
T. Kurt Bond <tkurtbond@gmail.com>
2024-05-09 15:08:38 -0400

    Add an explanatory comment for semicompatable {: :} for Gforth 0.7.3.

--- Src/Forth/Gforth/tkb.fs
+++ Src/Forth/Gforth/tkb.fs
@@ -1,6 +1,9 @@
 : POSSIBLY  ( "name" -- )  BL WORD FIND  ?dup AND IF  EXECUTE  THEN ;
 : anew  ( "name" -- )( Run: -- )  >in @ possibly  >in ! marker ;
 
+\ Forth 2012 compatible {: and :} using Gforth 0.7.3's { }, minus
+\ Forth 2012 |, which Gforth 0.7.3 doesn't understand.
+\ For {: a b | c -- :} use 0 {: a b c -- :} instead.
 ' { alias {: immediate
 locals-types definitions
 ' } alias :}
