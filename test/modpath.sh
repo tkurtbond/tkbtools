27cdeb9c8d6d61000771f2170e9d357785a66b9c
T. Kurt Bond <tkurtbond@gmail.com>
2024-12-04 13:28:17 -0500

    Add options to not exit on failure and to skip all but specified variant.

--- test/modpath.sh
+++ test/modpath.sh
@@ -1,7 +1,30 @@
 #! /usr/bin/env bash
 
+EXIT_ON_FAILURE=1
+MODPATH=''
+
+while getopts "aems" opt
+do
+    case "$opt" in
+        (\?) ((errors++)) ;;
+        (a) MODPATH=amodpath ;;
+        (e) EXIT_ON_FAILURE=0 ;;
+        (m) MODPATH=modpath ;;
+        (s) MODPATH=smodpath;;
+    esac
+done
+
+
 for i in Src/modpath Src/Scheme/Chicken/build/smodpath Src/Ada/bin/amodpath
 do
+    if [[ -n $MODPATH ]] && ! [[ $i =~ .*/$MODPATH ]]; then
+        echo skipping variant $i
+        continue
+    fi
     echo modpath variant $i
-    MODPATH=$i ./test/bats/bin/bats test/modpath.bats || exit
+    MODPATH=$i ./test/bats/bin/bats test/modpath.bats
+    EXIT_STATUS=$?
+    if ((EXIT_ON_FAILURE)) && ((EXIT_STATUS)); then
+        exit
+    fi
 done
