554e486d55791ef55561889c44998946bab94b46
T. Kurt Bond <tkurtbond@gmail.com>
2026-01-27 16:00:33 -0500

    Adapt afromunits command line handline to use Arg_Parser package.

--- Src/Ada/src/afromunits.adb
+++ Src/Ada/src/afromunits.adb
@@ -8,9 +8,13 @@ with Ada.Long_Long_Float_Text_IO; use Ada.Long_Long_Float_Text_IO;
 with GNAT.Command_Line; use GNAT.Command_Line;
 with GNAT.OS_Lib; use GNAT.OS_Lib;
 
+with Arg_Parser; use Arg_Parser;
+
 procedure AFromUnits is
    Program_Name : String :=
      (if Command_Name = "" then "affromunits" else Command_Name);
+
+   Exit_Program : exception;
 
    procedure Error (Message : String) is
    begin
@@ -108,7 +112,7 @@ procedure AFromUnits is
       end loop;
    end Print_SI_Prefixes;
 
-   procedure Print_Prefixes is
+   function Print_Prefixes return Boolean is
    begin
       Put_Line ("SI/Metric Prefixes: https://en.wikipedia.org/wiki/Metric_prefix");
       for C in Multipliers'Range loop
@@ -133,6 +137,7 @@ procedure AFromUnits is
          end;
       end loop;
 
+      return True;
    end Print_Prefixes;
 
    function Relaxed_From_String (S: String) return Long_Long_Float is
@@ -148,21 +153,22 @@ procedure AFromUnits is
          raise;
    end Relaxed_From_String;
 
-   Units : Unbounded_String := +"B"; -- Units default to bytes.
+   Units : aliased Unbounded_String := +"B"; -- Units default to bytes.
 
-   Number_Of_Arguments : Natural := 0;
+   Arguments_Seen : Natural := -0;
 
-   procedure Process_Argument (Arg: String) is
+   function Process_Argument (Start_With : Positive; Arg: String) return Boolean is
       S : String := Trim (Arg, Both);
+      R : Long_Long_Float;
       F : constant Integer := S'First;
       L : constant Integer := S'Last;
       I : Integer := S'First;
-      R : Long_Long_Float;
    begin
+      Arguments_Seen := Arguments_Seen + 1;
       -- Ugh.  This is so ugly.
       while I <= L and then (Is_Digit (S(I))
-                               or else S(I) = '_' or else S(I) = '.'
-                               or else S(I) = '+' or else S(I) = '-')
+                             or else S(I) = '_' or else S(I) = '.'
+                             or else S(I) = '+' or else S(I) = '-')
       loop
          I := I + 1;
       end loop;
@@ -173,51 +179,48 @@ procedure AFromUnits is
       end if;
       Put (R, Aft => 1, Exp => 0);
       New_Line;
+      return True;
    end Process_Argument;
 
    procedure Process_Standard_Input is
+      Continue : Boolean := True;
    begin
       loop
-         exit when End_Of_File;
-         Process_Argument (Get_Line);
+         exit when End_Of_File or not Continue;
+         Continue := Process_Argument (1, Get_Line);
       end loop;
    end Process_Standard_Input;
 
-   procedure Print_Usage is
+   function Print_Usage return Boolean;
+
+   Options : aliased Option_Array :=
+     (Make_Set_Boolean_False_Option
+        ("Use the binary prefixes for units.", 'b', "binary", Use_SI'Unrestricted_Access),
+      Make_Option
+        ("Display the help message.", 'h', "help", Print_Usage'Unrestricted_access),
+      Make_Option
+        ("Print the prefixes.", 'p', "prefixes", Print_Prefixes'Unrestricted_access),
+      Make_Set_Boolean_True_Option
+        ("Use the SI prefixes for units.", 's', "si", Use_Si'Unrestricted_Access),
+      Make_Set_Unbounded_String_Option
+        ("Set the units to use.", 'u', "units", Units'Unrestricted_access));
+
+   AP : Argument_Parser :=
+     Make_Argument_Parser ("afromunits [options] [argument ...]", Process_Argument'Unrestricted_Access,
+                           Options'Unrestricted_Access);
+   function Print_Usage return Boolean is
    begin
       Put_Line ("usage: " & Program_Name & " [option ...] [value ...]");
+      Usage (AP);
       New_Line;
+      raise Exit_Program;
+      return False;             -- If they ask for help, it's time to stop parsing arguments.
    end Print_Usage;
 
 begin
-   loop
-      declare
-         Ch: Character := Getopt ("- b h p s u:");
-      begin
-         case Ch is
-            when ASCII.NUL | '-' => exit;
-            when 'b' => Use_SI := False;
-            when 'h' => Print_Usage; GNAT.OS_Lib.OS_Exit (1);
-            when 'p' => Print_Prefixes;
-            when 's' => Use_SI := True;
-            when 'u' => Units := +Parameter;
-            when others =>
-               Fatal_Error (1, "unhandled option -" & Ch);
-         end case;
-      end;
-   end loop;
+   Parse_Arguments (AP);
 
-   loop
-      declare
-         Arg : constant String := Get_Argument;
-      begin
-         exit when Arg'Length = 0;
-         Number_Of_Arguments := Number_Of_Arguments + 1;
-         Process_Argument (Arg);
-      end;
-   end loop;
-
-   if Number_Of_Arguments < 1 then
+   if Arguments_Seen < 1 then
       Process_Standard_Input;
    end if;
 end AFromUnits;
